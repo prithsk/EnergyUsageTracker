98349911c85c66f0b60a92708ea5b6a93d8297f4
Prithvi Krishnan <prithvisk2023@gmail.com>
2025-02-07 08:27:51 -0600

    Coded the About Page

--- EnergyUsageTracker/About.cs
+++ EnergyUsageTracker/About.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,30 @@ namespace EnergyUsageTracker
         public About()
         {
             InitializeComponent();
+        }
+
+        private void btnRationale_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Thread t = new Thread(new ThreadStart(ThreadRationale));
+            t.Start();
+        }
+
+        private void ThreadRationale()
+        {
+            Application.Run(new Rationale());
+        }
+
+        private void btnHistory_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Thread t = new Thread(new ThreadStart(ThreadbtnHistory));
+            t.Start();
+        }
+
+        private void ThreadbtnHistory()
+        {
+            Application.Run(new History());
         }
     }
 }

bff5ab04e806a6912cb4e849b293981398c5f170
prithvisk <prithvisk2023@gmail.com>
2025-02-11 10:23:56 -0600

    UI Completed for Add Goals

--- EnergyUsageTracker/AddGoals.Designer.cs
+++ EnergyUsageTracker/AddGoals.Designer.cs
@@ -34,6 +34,18 @@
             this.btnback12 = new System.Windows.Forms.Button();
             this.labeHome = new System.Windows.Forms.Label();
             this.labSetGoals = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblNameGoal = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.btnAddGoal = new System.Windows.Forms.Button();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // labelAddGoals
@@ -88,12 +100,122 @@
             this.labSetGoals.TabIndex = 27;
             this.labSetGoals.Text = "Set Goals";
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(198, 95);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(418, 343);
+            this.dataGridView1.TabIndex = 28;
+            // 
+            // lblNameGoal
+            // 
+            this.lblNameGoal.AutoSize = true;
+            this.lblNameGoal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblNameGoal.Location = new System.Drawing.Point(36, 124);
+            this.lblNameGoal.Name = "lblNameGoal";
+            this.lblNameGoal.Size = new System.Drawing.Size(111, 20);
+            this.lblNameGoal.TabIndex = 29;
+            this.lblNameGoal.Text = "Name of Goal:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(47, 192);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 20);
+            this.label2.TabIndex = 30;
+            this.label2.Text = "Progress:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(50, 267);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 20);
+            this.label3.TabIndex = 31;
+            this.label3.Text = "Difficulty:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 157);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 20);
+            this.textBox1.TabIndex = 32;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(28, 224);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(140, 20);
+            this.textBox2.TabIndex = 33;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(28, 302);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(140, 20);
+            this.textBox3.TabIndex = 34;
+            // 
+            // btnAddGoal
+            // 
+            this.btnAddGoal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnAddGoal.Location = new System.Drawing.Point(40, 352);
+            this.btnAddGoal.Name = "btnAddGoal";
+            this.btnAddGoal.Size = new System.Drawing.Size(95, 34);
+            this.btnAddGoal.TabIndex = 35;
+            this.btnAddGoal.Text = "Add Goal";
+            this.btnAddGoal.UseVisualStyleBackColor = true;
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnPrev.Location = new System.Drawing.Point(656, 117);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(95, 34);
+            this.btnPrev.TabIndex = 36;
+            this.btnPrev.Text = "Previous";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnNext.Location = new System.Drawing.Point(656, 352);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(95, 34);
+            this.btnNext.TabIndex = 37;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.AutoSize = true;
+            this.lblDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.lblDisplay.Location = new System.Drawing.Point(313, 61);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(169, 22);
+            this.lblDisplay.TabIndex = 38;
+            this.lblDisplay.Text = "Displaying 5 Entries";
+            // 
             // AddGoals
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Aqua;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDisplay);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrev);
+            this.Controls.Add(this.btnAddGoal);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblNameGoal);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.labSetGoals);
             this.Controls.Add(this.labeHome);
             this.Controls.Add(this.btnback13);
@@ -101,6 +223,7 @@
             this.Controls.Add(this.labelAddGoals);
             this.Name = "AddGoals";
             this.Text = "AddGoals";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -113,5 +236,16 @@
         private System.Windows.Forms.Button btnback12;
         private System.Windows.Forms.Label labeHome;
         private System.Windows.Forms.Label labSetGoals;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblNameGoal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button btnAddGoal;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Label lblDisplay;
     }
 }
